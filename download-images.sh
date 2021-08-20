6f7c5f4f52f7dae498e4018791164c5d5469410d
Gustavo Zanatta Bruno <31906799+zanattabruno@users.noreply.github.com>
2021-08-20 17:30:40 -0400

    Insert script to download images

--- download-images.sh
+++ download-images.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+#Download Free5GC core images:
+
+docker push zanattabruno/mongo
+docker push zanattabruno/core_webapp
+docker push zanattabruno/core_upf
+docker push zanattabruno/core_smf
+docker push zanattabruno/core_hss
+docker push zanattabruno/core_amf
+docker push zanattabruno/core_pcrf
+
+#Download Free5GC core images:
+
+docker push zanattabruno/cudurunfapi_ru_nfapi
+docker push zanattabruno/cudurunfapi_du_nfapi
+docker push zanattabruno/cudurunfapi_cu_nfapi
